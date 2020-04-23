cd5e6d7d9415eaaecc7c2741ccec8fb445515bde
Akhmad Rizki Prayoga <akhmadrizki24@gmail.com>
2020-04-23 17:28:30 +0800

    electron part 8

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ ipcMain.on("appointment:create", (event, appointment) => {
     appointment["id"] = uuid();
     appointment["done"] = 0;
     allAppointment.push(appointment);
-
+    sendTodayAppointments();
     createWindow.close();
 
     console.log(allAppointment);
@@ -83,13 +83,23 @@ ipcMain.on("appointment:request:list", event => {
     listWindow.webContents.send('appointment:response:list', allAppointment);
 });
 
-ipcMain.on("appointment:request:index", event => {
+ipcMain.on("appointment:request:today", event => {
+    sendTodayAppointments();
     console.log("here2");
 });
 
 ipcMain.on("appointment:done", (event, id) => {
     console.log("here3");
 });
+
+const sendTodayAppointments = () => {
+    const today = new Date().toISOString().slice(0,10);
+    const filtered = allAppointment.filter(
+        appointment => appointment.date === today
+    );
+
+    todayWindow.webContents.send("appointment:response:today", filtered);
+}
 
 const menuTemplate = [{
     label: "File",
